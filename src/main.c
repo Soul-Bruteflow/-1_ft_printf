e541a130bc42643a6387d136625418c227b7eb9f
Max Vlad <mvlad@e3r6p5.unit.ua>
2017-10-16 13:56:49 +0300

    n.1.1.4

--- src/main.c
+++ src/main.c
@@ -92,8 +92,11 @@ int main()
 //	sys = printf("42Kashim a 1001 histoires à raconterIl fait au moins -8000002147483647-21474836482147483647-2147483648%d\n", 0000042);
 //	own = ft_printf("42Kashim a 1001 histoires à raconterIl fait au moins -8000002147483647-21474836482147483647-2147483648%d\n", 0000042);
 //
-	sys = printf("42Kashim a 1001 histoires à raconterIl fait au moins -8000\n002147483647-21474836482147483647-2147483648%d 0000042\n", -2147483648);
-	own = ft_printf("42Kashim a 1001 histoires à raconterIl fait au moins -8000\n002147483647-21474836482147483647-2147483648%d 0000042\n", -2147483648);
+//	sys = printf("42Kashim a 1001 histoires à raconterIl fait au moins -8000\n002147483647-21474836482147483647-2147483648%d 0000042\n", -2147483648);
+//	own = ft_printf("42Kashim a 1001 histoires à raconterIl fait au moins -8000\n002147483647-21474836482147483647-2147483648%d 0000042\n", -2147483648);
+
+	sys = printf("%d\n", 9);
+	own = ft_printf("%d\n", 9);
 
 //	sys =  printf("|%5h hi|\t|%20l li|\n", SHRT_MIN,LONG_MIN);
 //	own =  ft_printf("|%5h hi|\t|%20l li|\n", SHRT_MIN,LONG_MIN);
